73d0d50addf4d0afc6777e813f6ec2eb0402806b
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2022-11-09 19:12:11 +0300

    included new content

--- vote/scripts/script.js
+++ vote/scripts/script.js
@@ -3,7 +3,32 @@ $(document).ready(() => {
         $.get("./pages/home.html",
             function (data, textStatus, jqXHR) {
                 $(".overall-container").html(data)
-            },
+                homeManager()
+            }
         );
     })
 });
+
+function homeManager(){
+    let homeIcon = $("#menu > i")
+    $("#menu").click(() => {
+        $(homeIcon).toggleClass("fa-close", "fa-bars");
+        $(".basic-nav").toggleClass("hidden", "flex")
+    })
+    $(".basic-nav > span").each((index, icon) => {
+        $(icon).click(() => {
+            $(homeIcon).toggleClass("fa-close", "fa-bars");
+            $(".basic-nav").toggleClass("hidden", "flex")
+        })
+    })
+    $(".foot-item").each((index, element) => {
+        $(element).click(() =>{
+            let id = $(element).attr("id");
+            $.get(`../pages/${id}.html`,
+                (data, responseState, xhr) => {
+                    $(".home-main").html(data)
+                }
+            )
+        })
+    })
+}
